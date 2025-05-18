76362bee694e26c699fbe263df97d0e8f8ef6403
Hernán Sánchez <hsanchezalarcon@gmail.com>
2025-05-18 16:47:03 -0500

    ActoresOk

--- Seguridad/ActoresE.sql
+++ Seguridad/ActoresE.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE PACKAGE PKG_ADMINISTRADOR IS
     PROCEDURE ad_proveedor(nombre VARCHAR, direccion VARCHAR, telefono VARCHAR, correo VARCHAR);
     FUNCTION co_ventas_totales_mes;
-    FUNCTION co_mejor_proovedor_producto;
+    FUNCTION co_mejor_proovedor_producto(XidProducto VARCHAR) RETURN SYS_REFCURSOR;
     FUNCTION co_lista_empleados(XidSede VARCHAR) RETURN SYS_REFCURSOR;
     FUNCTION co_lista_valoraciones_bajas RETURN SYS_REFCURSOR;
     FUNCTION co_proveedores RETURN SYS_REFCURSOR;
@@ -19,10 +19,10 @@ CREATE OR REPLACE PACKAGE PKG_VENDEDOR IS
     PROCEDURE ad_detale_pedido(cantidad INTEGER, precio INTEGER, idPedido INTEGER, idProducto VARCHAR);
     PROCEDURE ad_cliente(tipo VARCHAR, numero INTEGER, nombre VARCHAR, direccion VARCHAR, telefono VARCHAR, correo VARCHAR,fechaNacimiento DATE);
     PROCEDURE ad_envio(fechaEnvio DATE, empresaTransporte VARCHAR, estado CHAR, direccionEnvio VARCHAR);
-    PROCEDURE co_precios RETURN SYS_REFCURSOR;
-    PROCEDURE co_venta RETURN SYS_REFCURSOR;
-    PROCEDURE co_producto RETURN SYS_REFCURSOR;
-    PROCEDURE co_cliente(n in varchar);
+    FUNCTION co_precio_producto (XidProducto VARCHAR)RETURN SYS_REFCURSOR;
+    FUNCTION co_venta (XidVenta INTEGER) RETURN SYS_REFCURSOR;
+    FUNCTION co_productos RETURN SYS_REFCURSOR;
+    FUNCTION co_cliente(Xnumero INTEGER) RETURN SYS_REFCURSOR;
     PROCEDURE mo_producto(XidProducto VARCHAR, XprecioCompra INTEGER , XprecioVenta INTEGER);
     PROCEDURE mo_cliente(Xcedula INTEGER, Xtipo VARCHAR, Xtelefono VARCHAR, Xdireccion VARCHAR, Xcorreo VARCHAR);
     PROCEDURE mo_pedido(XidPedido INTEGER, estado CHAR);
@@ -31,26 +31,26 @@ END PKG_VENDEDOR;
 
 -- PK ALAMACENISTA
 CREATE OR REPLACE PACKAGE PKG_ALMACENISTA IS
-    PROCEDURE co_pedidos_pend_proveedor();
-    PROCEDURE co_pedido(XidPedido INTEGER) RETURN SYS_REFCURSOR;
-    procedure co_detalle_pedido(xidPedido INTEGER) RETURN SYS_REFCURSOR;
     PROCEDURE ad_envio(fechaEnvio DATE, empresaTransporte VARCHAR, estado CHAR, direccionEnvio VARCHAR);
-    PROCEDURE co_envio();
-    PROCEDURE mo_envio();
+    PROCEDURE mo_envio(XidEnvio INTEGER,xEstado CHAR);
+    FUNCTION co_envio(XidEnvio INTEGER) RETURN SYS_REFCURSOR;
+    FUNCTION co_pedidos_pend_proveedor RETURN SYS_REFCURSOR;
+    FUNCTION co_pedido(XidPedido INTEGER) RETURN SYS_REFCURSOR;
+    FUNCTION co_detalle_pedido(xidPedido INTEGER) RETURN SYS_REFCURSOR;
 END PKG_ALMACENISTA;
 
 -- PK CLIENTE
 CREATE OR REPLACE PACKAGE PKG_CLIENTE IS
     PROCEDURE ad_envio(fechaEnvio DATE, empresaTransporte VARCHAR, estado CHAR, direccionEnvio VARCHAR);
-    PROCEDURE co_productos(EMPTY VARCHAR) RETURN SYS_REFCURSOR;
-    PROCEDURE co_envio();
+    FUNCTION co_productos RETURN SYS_REFCURSOR;
+    FUNCTION co_envio(XidEnvio INTEGER) RETURN SYS_REFCURSOR;
 END PKG_CLIENTE;
 
 -- PK ANALISTA DE NEGOCIOS
 CREATE OR REPLACE PACKAGE PKG_ANALISTA_NEGOCIOS IS
-    PROCEDURE co_ventas_totales_mes();
+    FUNCTION co_ventas_totales_mes RETURN SYS_REFCURSOR;
     PROCEDURE co_mejor_proovedor_producto(EMPTY VARCHAR);
-    PROCEDURE co_lista_empleados_sede(XidSede VARCHAR) RETURN SYS_REFCURSOR;
-    PROCEDURE co_lista_valoraciones_bajas(EMPTY VARCHAR) RETURN SYS_REFCURSOR;
-    PROCEDURE co_productos_mas_vendidos();
+    FUNCTION co_lista_empleados_sede(XidSede VARCHAR) RETURN SYS_REFCURSOR;
+    FUNCTION co_lista_valoraciones_bajas RETURN SYS_REFCURSOR;
+    FUNCTION co_productos_mas_vendidos RETURN SYS_REFCURSOR;
 END PKG_ANALISTA_NEGOCIOS;
